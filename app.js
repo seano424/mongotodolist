71f96cd11c3320828102b99dbed3a2665b65e96e
Sean OReilly <soreilly424@gmail.com>
2020-06-23 10:36:52 -0500

    changed app.listen

--- app.js
+++ app.js
@@ -128,7 +128,11 @@ app.post('/delete', function(req, res) {
       });
   }
 })
+// 
+// app.listen(port, function() {
+//   console.log(`Server Starts on ${port}`)
+// });
 
-app.listen(port, function() {
-  console.log(`Server Starts on ${port}`)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
